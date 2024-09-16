05776af08a0631069828166f32431f42799d2417
Kristian <kristianhadberg@gmail.com>
2024-09-16 12:06:14 +0200

    almost done with singly linked list

---
+++ datastructure-portfolio/linkedlist/singlylinkedlist.js
@@ -0,0 +1,20 @@
+class Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+export default class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add(data) {}
+
+  remove(data) {}
+
+  getFirst() {}
+
+  getLast() {}
+}
