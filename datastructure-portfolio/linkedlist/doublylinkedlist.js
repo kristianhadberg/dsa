1761a0eeb5cd5046ff6865142fa02a664f4aba1f
Kristian <kristianhadberg@gmail.com>
2024-09-29 18:15:22 +0200

    doubly linked list 75% done

---
+++ datastructure-portfolio/linkedlist/doublylinkedlist.js
@@ -0,0 +1,192 @@
+export class Node {
+  data = null;
+  next = null;
+  prev = null;
+  constructor(data) {
+    this.data = data;
+  }
+}
+
+export class DoublyLinkedList {
+  head = null;
+  tail = null;
+
+  addFirst(data) {
+    const newNode = new Node(data);
+    this.addNodeFirst(newNode);
+  }
+
+  addLast(data) {
+    const newNode = new Node(data);
+    this.addNodeLast(newNode);
+  }
+
+  first() {
+    return this.head;
+  }
+
+  last() {
+    let last;
+    this.head == this.tail ? (last = this.head) : (last = this.tail);
+
+    return last;
+  }
+
+  addNodeFirst(node) {
+    // if head doesnt exists (no elements in list)
+    if (this.head == null) {
+      this.makeNodeHead(node);
+      return;
+    }
+
+    if (this.head != null) {
+      const prevHead = this.head;
+      node.next = prevHead;
+      prevHead.prev = node;
+      this.makeNodeHead(node);
+    }
+  }
+
+  addNodeLast(node) {
+    // if head doesnt exists (no elements in list)
+    if (this.head == null) {
+      this.makeNodeHead(node);
+      return;
+    }
+
+    // if head exists but tail is null
+    if (this.tail == null && this.head != null) {
+      node.prev = this.head;
+      this.head.next = node;
+      this.tail = node;
+    }
+
+    // if head exists
+    if (this.tail != null) {
+      const prevTail = this.tail;
+      prevTail.next = node;
+      node.prev = prevTail;
+      this.tail = node;
+    }
+  }
+
+  removeNode(node) {
+    let current = this.head;
+    let previousNode;
+
+    while (current != null) {
+      if (current == node) {
+        if (node == this.head) {
+          if (this.head.next != null) {
+            this.makeNodeHead(this.head.next);
+            return node;
+          } else {
+            this.head = null;
+            return node;
+          }
+        }
+
+        if (current.next != null) {
+          previousNode.next = current.next;
+          current.next.prev = previousNode;
+          return node;
+        } else {
+          previousNode.next = null;
+          this.makeNodeTail(previousNode);
+          return node;
+        }
+      }
+
+      previousNode = current;
+      current = current.next;
+    }
+  }
+
+  removeFirst() {
+    let removedNode;
+    // if head has no next?
+    if (this.head == null) {
+      return null;
+    }
+
+    if (this.tail == null) {
+      removedNode = this.head;
+      this.head = null;
+      return removedNode;
+    } else {
+      removedNode = this.head;
+      const newHead = this.head.next;
+      this.makeNodeHead(newHead);
+      return removedNode;
+    }
+  }
+
+  removeLast() {
+    let removedNode;
+
+    // if head is null
+    if (this.head == null && this.tail) {
+      return null;
+    }
+
+    if (this.tail == null) {
+      removedNode = this.head;
+      this.head = null;
+      return removedNode;
+    }
+
+    if (this.head.next != this.tail) {
+      removedNode = this.tail;
+      this.makeNodeTail(removedNode.prev);
+      return removedNode;
+    }
+  }
+
+  /* 
+    Helps making node head by making sure head.prev is set to null
+  */
+  makeNodeHead(node) {
+    this.head = node;
+    this.head.prev = null;
+
+    if (this.head == this.tail) {
+      this.tail = null;
+    }
+  }
+
+  makeNodeTail(node) {
+    this.tail = node;
+    this.tail.next = null;
+  }
+
+  clear() {
+    /* 
+      loop through list and clear elements to save memory instead??
+    */
+    this.head = null;
+    this.tail = null;
+  }
+
+  size() {
+    let node = this.head;
+    let count = 0;
+
+    while (node != null) {
+      count++;
+
+      node = node.next;
+    }
+
+    return count;
+  }
+
+  dumpList() {
+    let node = this.head;
+
+    while (node != null) {
+      console.log(node);
+
+      node = node.next;
+    }
+  }
+}
---
+++ datastructure-portfolio/linkedlist/test_doublylinkedlist.js
@@ -0,0 +1,24 @@
+import { DoublyLinkedList, Node } from "./doublylinkedlist.js";
+
+const dlist = new DoublyLinkedList();
+const node = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+
+dlist.addNodeFirst(node);
+dlist.addNodeLast(node2);
+dlist.addNodeLast(node3);
+
+/* console.log(dlist.head);
+console.log(dlist.tail); */
+
+//console.log(dlist.removeFirst());
+//const res = dlist.removeLast();
+//dlist.dumpList();
+//console.log(dlist.removeNode(node2));
+dlist.removeNode(node);
+dlist.removeNode(node2);
+//dlist.removeNode(node3);
+dlist.dumpList();
+console.log(dlist.tail);
+//console.log(dlist.tail);
